ffcb5b7f141a1a1c33c83780025ba595485eb945
JIHONG KIM <64270116+ghk0409@users.noreply.github.com>
2023-03-22 15:39:46 +0900

    20230322_TypeORM 적용
    - posegresql DB 사용 설정
    - nestjs와 typerom을 통한 DB 연동
    - 환경변수 설정 (.env, cross-env)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,44 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 // import { join } from 'path';
 import { RestaurantsModule } from './restaurants/restaurants.module';
+import * as Joi from 'joi';
 
 @Module({
     imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath:
+                process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+            ignoreEnvFile: process.env.NODE_ENV === 'prod',
+            // 환경변수 데이터 유효성 검증
+            validationSchema: Joi.object({
+                NODE_ENV: Joi.string().valid('dev', 'prod').required(),
+                DB_HOST: Joi.string().required(),
+                DB_PORT: Joi.string().required(),
+                DB_USERNAME: Joi.string().required(),
+                DB_PASSWORD: Joi.string().required(),
+                DB_NAME: Joi.string().required(),
+            }),
+        }),
         GraphQLModule.forRoot<ApolloDriverConfig>({
             driver: ApolloDriver,
             // autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
             // schema.gql 파일 생성 없이 메모리 상으로 저장
             autoSchemaFile: true,
+        }),
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: process.env.DB_HOST,
+            port: +process.env.DB_PORT,
+            username: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD, // localhost 연결 시, postgres는 패스워드 필요 X
+            database: process.env.DB_NAME,
+            synchronize: true, // true일 경우: TypeORM이 DB에 연결할 때, DB를 모듈의 현재 상태로 마이그레이션함 (production에서는 true 금지!!)
+            logging: true,
         }),
         RestaurantsModule,
     ],
